d4297dbd6dc0f82fcc438df4a180b62c9660eef3
Amal <99046970+AmalGho@users.noreply.github.com>
2022-07-18 22:38:38 +0300

    add files

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-let body =document.getElementById('body');
-let btn = document.getElementById('new-quote');
-let linktweet = document.getElementById('tweet-quote');
-let linktumbler = document.getElementById('tumbler-quote');
-let text = document.getElementById('quote-text');
-let author = document.getElementById('quote-author');
+
 let textQ = document.getElementById('text');
 let authorQ = document.getElementById('author');
-
-
 
 
 function randomColor(num){
@@ -26,20 +19,3 @@ $(function() {
         });
     });
 });
-
-// function newQuote(){
-//     const color = 'rgb(' + randomColor(255) + ',' + randomColor(255) + ',' + randomColor(255) + ')';
-//     body.style.backgroundColor=color;
-//     btn.style.backgroundColor= color;
-//     linktweet.style.backgroundColor=color;
-//     linktumbler.style.backgroundColor=color;
-//     text.style.color=color;
-//     author.style.color=color;
-
-//     let index = qoutArray[Math.floor(Math.random()*qoutArray.length)];
-
-//     textQ.innerText=index.text;
-//     authorQ.innerText=index.author;
-
-//     return false;
-// }
